2cde75b059f2a91b52682f846cc1a0a83cc4d9a6
trikitrok <trikitrok@gmail.com>
2016-05-18 12:07:51 +0200

    initial code for part 1

---
+++ src/test/java/com/dodevjutsu/katas/ohce/tests/acceptance/RunningOhceShould.java
@@ -0,0 +1,14 @@
+package com.dodevjutsu.katas.ohce.tests.acceptance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RunningOhceShould {
+
+    @Test
+    public void shouldFail() {
+        assertEquals(false, true);
+    }
+
+}
